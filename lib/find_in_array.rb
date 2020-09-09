abe05158456b71e747ad95c5a6b16559f8e98b75
Kim Nguyen <nnhk23@gmail.com>
2020-09-09 23:18:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
     if array[index] == value_to_find
-      index
-    else nil
+      return index
+    else 
+      return nil
   end
 end
 end
